ad76547af6fcb9695446aeb03dc923a2c197f18d
shadow-0120 <benweidz33@gmail.com>
2025-10-24 18:05:07 +0100

    Add text-white class to main container for improved styling

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ export default function App() {
   
   return (
       <Router>
-        <div className="">
+        <div className="text-white">
           <PerformanceOptimizer />
           <Navbar />
           <main>
